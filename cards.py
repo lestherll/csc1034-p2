d39a277934e3a43eb0a1061ea6367abdb4d1a825
Lesther Llacuna (UG) <c0026034@newcastle.ac.uk>
2021-01-06 22:27:04 +0000

    Fixed duplicate instance of 2 at values attribute

--- cards.py
+++ cards.py
@@ -8,7 +8,7 @@ class Card(namedtuple('CardData', ['suit', 'value'])):
     A Card is a namedtuple with the fields 'suit' and 'value'.
     """
     suits = '♣ ♢ ♡ ♠'.split()
-    values = '2 2 3 4 5 6 7 8 9 10 J Q K A'.split()
+    values = '2 3 4 5 6 7 8 9 10 J Q K A'.split()
 
     def __new__(cls, suit, value):
         assert suit in cls.suits
